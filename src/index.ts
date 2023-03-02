4ec4eb55ed5fe4bb3dc42ce1e189e973658b17b8
Marcelo Ferreira <flaviomff@gmail.com>
2023-03-02 10:03:28 -0300

    feat: group by function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Index Page");
+import { mockData } from "../data/mock";
+
+const groupBy = (array: any[], key: string) => {
+  return array.reduce((acc, currentValue) => {
+    if (!acc[currentValue[key]]) {
+      acc[currentValue[key]] = [];
+    }
+    acc[currentValue[key]].push(currentValue);
+
+    return acc;
+  }, {});
+};
+
+console.log(groupBy(mockData, "state"));
+
